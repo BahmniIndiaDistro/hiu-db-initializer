df92c6c06597fbebc2d1a747c84fa71f33ae0a21
SanoferSameera <sanofersameera25@gmail.com>
2022-05-10 18:49:50 +0530

    BAH-1648 | Add. a query to create database if not exists

--- src/main/java/org/projecteka/hiu/Application.java
+++ src/main/java/org/projecteka/hiu/Application.java
@@ -10,13 +10,16 @@ import liquibase.resource.ClassLoaderResourceAccessor;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Creating schema for health information user");
+        System.out.println("Connecting...");
         try {
             Connection connection = openConnection();
             Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(connection));
+            System.out.println("Creating schema for health information user");
             Liquibase liquibase = new liquibase.Liquibase("liquibase.xml", new ClassLoaderResourceAccessor(), database);
             liquibase.update(new Contexts(), new LabelExpression());
         } catch (Exception e) {
@@ -28,10 +31,19 @@ public class Application {
         String jdbcUrl = System.getProperty("jdbc.url");
         String jdbcUsername = System.getProperty("jdbc.username");
         String jdbcUserPwd = System.getProperty("jdbc.password");
-        if (isEmptyString(jdbcUrl) || isEmptyString(jdbcUsername) || isEmptyString(jdbcUserPwd)) {
+        String jdbcDatabase = System.getProperty("jdbc.database");
+        if (isEmptyString(jdbcUrl) || isEmptyString(jdbcUsername) || isEmptyString(jdbcUserPwd) || isEmptyString(jdbcDatabase)) {
             throw new Exception("you must set jdbc.url, jdbc.username, jdbc.password as properties, either using -D option or setting as env");
         }
-        return DriverManager.getConnection(jdbcUrl, jdbcUsername, jdbcUserPwd);
+        Connection connection = DriverManager.getConnection(jdbcUrl, jdbcUsername, jdbcUserPwd);
+        Statement stmt = connection.createStatement( ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+        ResultSet rs = stmt.executeQuery(String.format("SELECT FROM pg_database WHERE datname = '%s'", jdbcDatabase));
+        rs.last();
+        if(rs.getRow() == 0){
+            System.out.println("Creating database for health information user");
+            stmt.executeUpdate(String.format("CREATE DATABASE %s", jdbcDatabase));
+        }
+        return DriverManager.getConnection(jdbcUrl.concat(jdbcDatabase), jdbcUsername, jdbcUserPwd);
     }
 
     private static boolean isEmptyString(String value) {
